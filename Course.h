5b9d520bdf3bad6f22ea68af6630c134c6fbdbea
habibagharib <habiba.gharib@student.giu-uni.de>
2025-11-11 09:08:43 +0200

    Course.h

--- Course.h
+++ Course.h
@@ -4,7 +4,16 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+class Course {
+private:
+String courseCode;
+String courseName;
+int maxStudents;
+Student* students;
+int currentStudents;
+public:
+addStudent(const Student& s);
+displayCourseInfo();
 
 
 
